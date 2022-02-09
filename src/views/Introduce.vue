cc591d1963e76c121354fed1c1b78f800bb82e4d
xingyu-Lu <2273466984@qq.com>
2022-02-09 17:44:53 +0800

    update

--- src/views/Introduce.vue
+++ src/views/Introduce.vue
@@ -2,7 +2,7 @@
 	<el-card class="account-container" style="display: flex; justify-content: flex-start; align-items: center;">
 		<h1>系统简介</h1>
 		<div style="line-height: 30px">
-			后台管理系统，基于 Laravel 、Vue 3.0 、Element Plus 相关技术栈开发的医院数据统计分析管理系统。
+			后台管理系统，基于 Laravel 、Vue 3.0 、Element Plus 相关技术栈开发的医院数据统计分析运营管理系统。
 		</div>
 		<h1>主要功能</h1>
 		<ul style="font-weight: bold; line-height: 32px;">
@@ -23,13 +23,130 @@
 			<li>axios</li>
 		</ul>
 	</el-card>
+	<!-- <div id="zoom"></div> -->
 </template>
 
-<script>	
+<script>
+	import {
+		onMounted,
+		onUnmounted
+	} from 'vue'
+	import * as echarts from 'echarts';
+
+	let myChart = null
 	export default {
-		name: 'Introduce'
+		name: 'Introduce',
+		setup() {
+			onMounted(() => {
+				// 基于准备好的dom，初始化echarts实例
+				myChart = echarts.init(document.getElementById('zoom'))
+
+				// 指定图表的配置项和数据
+				const option = {
+					title: {
+						text: '系统折线图'
+					},
+					tooltip: {
+						trigger: 'axis',
+						axisPointer: {
+							type: 'cross',
+							label: {
+								backgroundColor: '#6a7985'
+							}
+						}
+					},
+					legend: {
+						data: ['新增注册', '付费用户', '活跃用户', '订单数', '当日总收入']
+					},
+					toolbox: {
+						feature: {
+							saveAsImage: {}
+						}
+					},
+					grid: {
+						left: '3%',
+						right: '4%',
+						bottom: '3%',
+						containLabel: true
+					},
+					xAxis: [{
+						type: 'category',
+						boundaryGap: false,
+						data: ['2021-03-11', '2021-03-12', '2021-03-13', '2021-03-14',
+							'2021-03-15', '2021-03-16', '2021-03-17'
+						]
+					}],
+					yAxis: [{
+						type: 'value'
+					}],
+					series: [{
+							name: '新增注册',
+							type: 'line',
+							stack: '总量',
+							areaStyle: {},
+							emphasis: {
+								focus: 'series'
+							},
+							data: [120, 132, 101, 134, 90, 230, 210]
+						},
+						{
+							name: '付费用户',
+							type: 'line',
+							stack: '总量',
+							areaStyle: {},
+							emphasis: {
+								focus: 'series'
+							},
+							data: [220, 182, 191, 234, 290, 330, 310]
+						},
+						{
+							name: '活跃用户',
+							type: 'line',
+							stack: '总量',
+							areaStyle: {},
+							emphasis: {
+								focus: 'series'
+							},
+							data: [150, 232, 201, 154, 190, 330, 410]
+						},
+						{
+							name: '订单数',
+							type: 'line',
+							stack: '总量',
+							areaStyle: {},
+							emphasis: {
+								focus: 'series'
+							},
+							data: [320, 332, 301, 334, 390, 330, 320]
+						},
+						{
+							name: '当日总收入',
+							type: 'line',
+							stack: '总量',
+							label: {
+								show: true,
+								position: 'top'
+							},
+							areaStyle: {},
+							emphasis: {
+								focus: 'series'
+							},
+							data: [820, 932, 901, 934, 1290, 1330, 1320]
+						}
+					]
+				}
+
+				// 使用刚指定的配置项和数据显示图表。
+				myChart.setOption(option)
+			})
+			onUnmounted(() => {
+				myChart.dispose()
+			})
+		}
 	}
 </script>
 <style scoped>
-
+	#zoom {
+		min-height: 300px;
+	}
 </style>

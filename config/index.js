d3648ff9cc04d9f367589fc778d60fd8e914b2f7
xingyu-Lu <2273466984@qq.com>
2022-02-09 15:45:04 +0800

    update

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ export default {
     baseUrl: '/apis' // 测试接口域名
   },
   production: {
-    baseUrl: 'http://120.26.124.151:8001' // 正式接口域名
+    baseUrl: 'http://120.26.124.151:9001' // 正式接口域名
   }
 }

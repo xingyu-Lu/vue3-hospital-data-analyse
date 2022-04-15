977d91b25eb571c003d9519273269d6f777d359f
xingyu-Lu <2273466984@qq.com>
2022-04-15 20:34:34 +0800

    update

--- config/index.js
+++ config/index.js
@@ -2,6 +2,9 @@ export default {
   development: {
     baseUrl: '/apis' // 测试接口域名
   },
+  test: {
+    baseUrl: 'http://120.26.124.151:9001' // 正式接口域名
+  },
   production: {
     baseUrl: 'http://120.26.124.151:9001' // 正式接口域名
   }
--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ import {
 
 const baseUrl = {
 	development: './',
+	test: './',
 	production: './',
 }
 
